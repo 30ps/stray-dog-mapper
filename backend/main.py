8cb2a9068509d312e2f8e87dcc64d7aa4067d763
30ps <panagiotis.triantafyllou@e-food.gr>
2025-07-17 15:01:13 +0300

    removed functions_framework

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,5 @@
 from fastapi import FastAPI
 from app.routes import router
-from starlette.requests import Request
-from starlette.responses import Response
 
 app = FastAPI()
 app.include_router(router)
-
-@functions_framework.http
-def stray_dog_mapper(request):
-    """
-    Cloud Run entrypoint for FastAPI app using functions_framework.
-    """
-    # Convert Flask request to ASGI request
-    asgi_request = Request(request.environ)
-    response = Response()
-    return app(asgi_request.scope, asgi_request.receive, response.send)
